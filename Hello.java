ad9811b6a264c9f321358389e3622d7ae3839868
unknown <engineermithu>
2021-03-12 23:08:05 +0600

    Added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.printIn("Vs Code is Awesome...");
+        greeting("Mithu Hasan");
     } 
         public static greeting (String name) {
-            System.out.printIn("");
+            System.out.printIn("Welcome" + name);
 
         }
     
