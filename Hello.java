d3d352514a153b78bdb927f0ad82d96cb8c39733
unknown <engineermithu>
2021-03-14 00:18:03 +0600

    all method remove and add message method

--- Hello.java
+++ Hello.java
@@ -1,15 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.printIn("Vs Code is Awesome...");
-        display();
     } 
 
-        public static display () {
-            System.out.printIn("Hello Hello");
-
-        }
-        public static int sqr(int x) {
-            return x*x;
-        }
+       public static void message() {
+           System.out.printIn("Hooray!!! Now we are able to conribute on open sourse.......");
+       }
+    
     
 }
